46080b5c82637e34b241d4f76bfe31af2e7554fa
hunglc007@gmail.com <hunglc007@gmail.com>
2020-04-27 08:56:06 +0700

    Update benchmarks file

---
+++ benchmarks.py
@@ -0,0 +1,51 @@
+import numpy as np
+import tensorflow as tf
+import time
+import cv2
+from core.yolov4 import YOLOv4, decode
+from absl import app, flags, logging
+from absl.flags import FLAGS
+from core import utils
+
+flags.DEFINE_string('weights', './data/yolov4.weights', 'path to weights file')
+flags.DEFINE_string('image', './data/kite.jpg', 'path to input image')
+flags.DEFINE_integer('size', 416, 'resize images to')
+
+def main(_argv):
+    input_layer = tf.keras.layers.Input([FLAGS.size, FLAGS.size, 3])
+    feature_maps = YOLOv4(input_layer)
+    bbox_tensors = []
+    for i, fm in enumerate(feature_maps):
+        bbox_tensor = decode(fm, i)
+        bbox_tensors.append(bbox_tensor)
+
+    model = tf.keras.Model(input_layer, bbox_tensors)
+    utils.load_weights(model, FLAGS.weights)
+    logging.info('weights loaded')
+
+    # Test the TensorFlow Lite model on random input data.
+    for i in range(1000):
+        img_raw = tf.image.decode_image(
+            open(FLAGS.image, 'rb').read(), channels=3)
+
+        original_image = cv2.imread(FLAGS.image)
+        original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+        original_image_size = original_image.shape[:2]
+        image_data = utils.image_preporcess(np.copy(original_image), [FLAGS.size, FLAGS.size])
+        image_data = image_data[np.newaxis, ...].astype(np.float32)
+
+        prev_time = time.time()
+        pred_bbox = model.predict(image_data)
+        # pred_bbox = pred_bbox.numpy()
+        curr_time = time.time()
+        exec_time = curr_time - prev_time
+
+        info = "time:" + str(round(1000 * exec_time, 2)) + " ms, FPS: " + str(round((1000 / (1000 * exec_time)), 1))
+        print(info)
+
+
+if __name__ == '__main__':
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
