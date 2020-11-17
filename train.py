a43d5d18a9dcb3b76b22f70709e86ba1479535e1
the-robot <9334746+the-robot@users.noreply.github.com>
2020-11-18 04:33:58 +0800

    Removed uncommented code

--- train.py
+++ train.py
@@ -65,99 +65,99 @@ def main(_argv):
     model = tf.keras.Model(input_layer, bbox_tensors)
     model.summary()
 
-    # if FLAGS.weights == None:
-    #     print("Training from scratch")
-    # else:
-    #     if FLAGS.weights.split(".")[len(FLAGS.weights.split(".")) - 1] == "weights":
-    #         utils.load_weights(model, FLAGS.weights, FLAGS.model, FLAGS.tiny)
-    #     else:
-    #         model.load_weights(FLAGS.weights)
-    #     print('Restoring weights from: %s ... ' % FLAGS.weights)
+    if FLAGS.weights == None:
+        print("Training from scratch")
+    else:
+        if FLAGS.weights.split(".")[len(FLAGS.weights.split(".")) - 1] == "weights":
+            utils.load_weights(model, FLAGS.weights, FLAGS.model, FLAGS.tiny)
+        else:
+            model.load_weights(FLAGS.weights)
+        print('Restoring weights from: %s ... ' % FLAGS.weights)
 
 
-    # optimizer = tf.keras.optimizers.Adam()
-    # if os.path.exists(logdir): shutil.rmtree(logdir)
-    # writer = tf.summary.create_file_writer(logdir)
+    optimizer = tf.keras.optimizers.Adam()
+    if os.path.exists(logdir): shutil.rmtree(logdir)
+    writer = tf.summary.create_file_writer(logdir)
 
-    # # define training step function
-    # # @tf.function
-    # def train_step(image_data, target):
-    #     with tf.GradientTape() as tape:
-    #         pred_result = model(image_data, training=True)
-    #         giou_loss = conf_loss = prob_loss = 0
+    # define training step function
+    # @tf.function
+    def train_step(image_data, target):
+        with tf.GradientTape() as tape:
+            pred_result = model(image_data, training=True)
+            giou_loss = conf_loss = prob_loss = 0
 
-    #         # optimizing process
-    #         for i in range(len(freeze_layers)):
-    #             conv, pred = pred_result[i * 2], pred_result[i * 2 + 1]
-    #             loss_items = compute_loss(pred, conv, target[i][0], target[i][1], STRIDES=STRIDES, NUM_CLASS=NUM_CLASS, IOU_LOSS_THRESH=IOU_LOSS_THRESH, i=i)
-    #             giou_loss += loss_items[0]
-    #             conf_loss += loss_items[1]
-    #             prob_loss += loss_items[2]
+            # optimizing process
+            for i in range(len(freeze_layers)):
+                conv, pred = pred_result[i * 2], pred_result[i * 2 + 1]
+                loss_items = compute_loss(pred, conv, target[i][0], target[i][1], STRIDES=STRIDES, NUM_CLASS=NUM_CLASS, IOU_LOSS_THRESH=IOU_LOSS_THRESH, i=i)
+                giou_loss += loss_items[0]
+                conf_loss += loss_items[1]
+                prob_loss += loss_items[2]
 
-    #         total_loss = giou_loss + conf_loss + prob_loss
+            total_loss = giou_loss + conf_loss + prob_loss
 
-    #         gradients = tape.gradient(total_loss, model.trainable_variables)
-    #         optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-    #         tf.print("=> STEP %4d/%4d   lr: %.6f   giou_loss: %4.2f   conf_loss: %4.2f   "
-    #                  "prob_loss: %4.2f   total_loss: %4.2f" % (global_steps, total_steps, optimizer.lr.numpy(),
-    #                                                            giou_loss, conf_loss,
-    #                                                            prob_loss, total_loss))
-    #         # update learning rate
-    #         global_steps.assign_add(1)
-    #         if global_steps < warmup_steps:
-    #             lr = global_steps / warmup_steps * cfg.TRAIN.LR_INIT
-    #         else:
-    #             lr = cfg.TRAIN.LR_END + 0.5 * (cfg.TRAIN.LR_INIT - cfg.TRAIN.LR_END) * (
-    #                 (1 + tf.cos((global_steps - warmup_steps) / (total_steps - warmup_steps) * np.pi))
-    #             )
-    #         optimizer.lr.assign(lr.numpy())
+            gradients = tape.gradient(total_loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+            tf.print("=> STEP %4d/%4d   lr: %.6f   giou_loss: %4.2f   conf_loss: %4.2f   "
+                     "prob_loss: %4.2f   total_loss: %4.2f" % (global_steps, total_steps, optimizer.lr.numpy(),
+                                                               giou_loss, conf_loss,
+                                                               prob_loss, total_loss))
+            # update learning rate
+            global_steps.assign_add(1)
+            if global_steps < warmup_steps:
+                lr = global_steps / warmup_steps * cfg.TRAIN.LR_INIT
+            else:
+                lr = cfg.TRAIN.LR_END + 0.5 * (cfg.TRAIN.LR_INIT - cfg.TRAIN.LR_END) * (
+                    (1 + tf.cos((global_steps - warmup_steps) / (total_steps - warmup_steps) * np.pi))
+                )
+            optimizer.lr.assign(lr.numpy())
 
-    #         # writing summary data
-    #         with writer.as_default():
-    #             tf.summary.scalar("lr", optimizer.lr, step=global_steps)
-    #             tf.summary.scalar("loss/total_loss", total_loss, step=global_steps)
-    #             tf.summary.scalar("loss/giou_loss", giou_loss, step=global_steps)
-    #             tf.summary.scalar("loss/conf_loss", conf_loss, step=global_steps)
-    #             tf.summary.scalar("loss/prob_loss", prob_loss, step=global_steps)
-    #         writer.flush()
+            # writing summary data
+            with writer.as_default():
+                tf.summary.scalar("lr", optimizer.lr, step=global_steps)
+                tf.summary.scalar("loss/total_loss", total_loss, step=global_steps)
+                tf.summary.scalar("loss/giou_loss", giou_loss, step=global_steps)
+                tf.summary.scalar("loss/conf_loss", conf_loss, step=global_steps)
+                tf.summary.scalar("loss/prob_loss", prob_loss, step=global_steps)
+            writer.flush()
 
-    # def test_step(image_data, target):
-    #     with tf.GradientTape() as tape:
-    #         pred_result = model(image_data, training=True)
-    #         giou_loss = conf_loss = prob_loss = 0
+    def test_step(image_data, target):
+        with tf.GradientTape() as tape:
+            pred_result = model(image_data, training=True)
+            giou_loss = conf_loss = prob_loss = 0
 
-    #         # optimizing process
-    #         for i in range(len(freeze_layers)):
-    #             conv, pred = pred_result[i * 2], pred_result[i * 2 + 1]
-    #             loss_items = compute_loss(pred, conv, target[i][0], target[i][1], STRIDES=STRIDES, NUM_CLASS=NUM_CLASS, IOU_LOSS_THRESH=IOU_LOSS_THRESH, i=i)
-    #             giou_loss += loss_items[0]
-    #             conf_loss += loss_items[1]
-    #             prob_loss += loss_items[2]
+            # optimizing process
+            for i in range(len(freeze_layers)):
+                conv, pred = pred_result[i * 2], pred_result[i * 2 + 1]
+                loss_items = compute_loss(pred, conv, target[i][0], target[i][1], STRIDES=STRIDES, NUM_CLASS=NUM_CLASS, IOU_LOSS_THRESH=IOU_LOSS_THRESH, i=i)
+                giou_loss += loss_items[0]
+                conf_loss += loss_items[1]
+                prob_loss += loss_items[2]
 
-    #         total_loss = giou_loss + conf_loss + prob_loss
+            total_loss = giou_loss + conf_loss + prob_loss
 
-    #         tf.print("=> TEST STEP %4d   giou_loss: %4.2f   conf_loss: %4.2f   "
-    #                  "prob_loss: %4.2f   total_loss: %4.2f" % (global_steps, giou_loss, conf_loss,
-    #                                                            prob_loss, total_loss))
+            tf.print("=> TEST STEP %4d   giou_loss: %4.2f   conf_loss: %4.2f   "
+                     "prob_loss: %4.2f   total_loss: %4.2f" % (global_steps, giou_loss, conf_loss,
+                                                               prob_loss, total_loss))
 
-    # for epoch in range(first_stage_epochs + second_stage_epochs):
-    #     if epoch < first_stage_epochs:
-    #         if not isfreeze:
-    #             isfreeze = True
-    #             for name in freeze_layers:
-    #                 freeze = model.get_layer(name)
-    #                 utils.freeze_all(freeze)
-    #     elif epoch >= first_stage_epochs:
-    #         if isfreeze:
-    #             isfreeze = False
-    #             for name in freeze_layers:
-    #                 freeze = model.get_layer(name)
-    #                 utils.unfreeze_all(freeze)
-    #     for image_data, target in trainset:
-    #         train_step(image_data, target)
-    #     for image_data, target in testset:
-    #         test_step(image_data, target)
-    #     model.save_weights("./checkpoints/yolov4")
+    for epoch in range(first_stage_epochs + second_stage_epochs):
+        if epoch < first_stage_epochs:
+            if not isfreeze:
+                isfreeze = True
+                for name in freeze_layers:
+                    freeze = model.get_layer(name)
+                    utils.freeze_all(freeze)
+        elif epoch >= first_stage_epochs:
+            if isfreeze:
+                isfreeze = False
+                for name in freeze_layers:
+                    freeze = model.get_layer(name)
+                    utils.unfreeze_all(freeze)
+        for image_data, target in trainset:
+            train_step(image_data, target)
+        for image_data, target in testset:
+            test_step(image_data, target)
+        model.save_weights("./checkpoints/yolov4")
 
 
 if __name__ == '__main__':
