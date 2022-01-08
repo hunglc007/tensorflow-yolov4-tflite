4c2609b0097bd85a981cccf1dfcd0ad763d8bb70
manojmaximum <76239353+manojmaximum@users.noreply.github.com>
2022-01-08 08:18:25 +0530

    tfgpu version

--- detectvideo.py
+++ detectvideo.py
@@ -112,7 +112,7 @@ def main(_argv):
         result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         if not FLAGS.dis_cv2_window:
             # cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
-            cv2.imwrite(f"{time.time()}.jpg",result)
+            cv2.imwrite(f"./output/{time.time()}.jpg",result)
             # cv2.imshow("result", result)
             if cv2.waitKey(1) & 0xFF == ord('q'): break
 
