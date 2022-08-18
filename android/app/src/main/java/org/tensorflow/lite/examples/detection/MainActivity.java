43065f4c450ba6b032187227e6edd33bd630319f
ky3vin <kimpika8541@naver.com>
2022-08-18 22:23:08 +0900

    사용할 탐지 모델 변경

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
@@ -247,8 +247,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final boolean TF_OD_API_IS_QUANTIZED = false;
 
+//    private static final String TF_OD_API_MODEL_FILE = "yolov4-tiny-416.tflite";
     private static final String TF_OD_API_MODEL_FILE = "yolov4-416-fp32.tflite";
-
     private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/coco.txt";
 
     // Minimum detection confidence to track a detection.
