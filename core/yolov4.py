ed12b23dca8c6af6754d4cb0700f9e794869b5a2
Ankit Sachan <>
2022-08-24 19:07:51 +0530

    dev branch initialized

--- core/yolov4.py
+++ core/yolov4.py
@@ -296,6 +296,7 @@ def filter_boxes(box_xywh, scores, score_threshold=0.4, input_shape = tf.constan
     class_boxes = tf.boolean_mask(box_xywh, mask)
     pred_conf = tf.boolean_mask(scores, mask)
     class_boxes = tf.reshape(class_boxes, [tf.shape(scores)[0], -1, tf.shape(class_boxes)[-1]])
+    class_boxes = class_boxes/float(input_shape[0])
     pred_conf = tf.reshape(pred_conf, [tf.shape(scores)[0], -1, tf.shape(pred_conf)[-1]])
 
     box_xy, box_wh = tf.split(class_boxes, (2, 2), axis=-1)
@@ -305,16 +306,30 @@ def filter_boxes(box_xywh, scores, score_threshold=0.4, input_shape = tf.constan
     box_yx = box_xy[..., ::-1]
     box_hw = box_wh[..., ::-1]
 
-    box_mins = (box_yx - (box_hw / 2.)) / input_shape
-    box_maxes = (box_yx + (box_hw / 2.)) / input_shape
+    # box_mins = (box_yx - (box_hw / 2.)) / input_shape
+    # box_maxes = (box_yx + (box_hw / 2.)) / input_shape
+
+    box_mins = (box_yx) / input_shape
+    box_maxes = (box_hw) / input_shape
+
+
+    # boxes = tf.concat([
+    #     box_mins[..., 0:1],  # y_min
+    #     box_mins[..., 1:2],  # x_min
+    #     box_maxes[..., 0:1],  # y_max
+    #     box_maxes[..., 1:2]  # x_max
+    # ], axis=-1)
+
     boxes = tf.concat([
-        box_mins[..., 0:1],  # y_min
         box_mins[..., 1:2],  # x_min
-        box_maxes[..., 0:1],  # y_max
-        box_maxes[..., 1:2]  # x_max
+        box_mins[..., 0:1],  # y_min
+        box_maxes[..., 1:2],  # width
+        box_maxes[..., 0:1],  # height
+
     ], axis=-1)
+
     # return tf.concat([boxes, pred_conf], axis=-1)
-    return (boxes, pred_conf)
+    return (class_boxes, pred_conf)
 
 
 def compute_loss(pred, conv, label, bboxes, STRIDES, NUM_CLASS, IOU_LOSS_THRESH, i=0):
--- detect.py
+++ detect.py
@@ -5,6 +5,7 @@ if len(physical_devices) > 0:
 from absl import app, flags, logging
 from absl.flags import FLAGS
 import core.utils as utils
+from core.config import cfg
 from core.yolov4 import filter_boxes
 from tensorflow.python.saved_model import tag_constants
 from PIL import Image
@@ -19,10 +20,11 @@ flags.DEFINE_string('weights', './checkpoints/yolov4-416',
 flags.DEFINE_integer('size', 416, 'resize images to')
 flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
 flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
-flags.DEFINE_string('image', './data/kite.jpg', 'path to input image')
-flags.DEFINE_string('output', 'result.png', 'path to output image')
+flags.DEFINE_list('images', './data/images/kite.jpg', 'path to input image')
+flags.DEFINE_string('output', './detections/', 'path to output folder')
 flags.DEFINE_float('iou', 0.45, 'iou threshold')
 flags.DEFINE_float('score', 0.25, 'score threshold')
+flags.DEFINE_boolean('dont_show', False, 'dont show image output')
 
 def main(_argv):
     config = ConfigProto()
@@ -30,60 +32,75 @@ def main(_argv):
     session = InteractiveSession(config=config)
     STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
     input_size = FLAGS.size
-    image_path = FLAGS.image
+    images = FLAGS.images
 
-    original_image = cv2.imread(image_path)
-    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
-
-    # image_data = utils.image_preprocess(np.copy(original_image), [input_size, input_size])
-    image_data = cv2.resize(original_image, (input_size, input_size))
-    image_data = image_data / 255.
-    # image_data = image_data[np.newaxis, ...].astype(np.float32)
-
-    images_data = []
-    for i in range(1):
-        images_data.append(image_data)
-    images_data = np.asarray(images_data).astype(np.float32)
-
+    # load model
     if FLAGS.framework == 'tflite':
-        interpreter = tf.lite.Interpreter(model_path=FLAGS.weights)
-        interpreter.allocate_tensors()
-        input_details = interpreter.get_input_details()
-        output_details = interpreter.get_output_details()
-        print(input_details)
-        print(output_details)
-        interpreter.set_tensor(input_details[0]['index'], images_data)
-        interpreter.invoke()
-        pred = [interpreter.get_tensor(output_details[i]['index']) for i in range(len(output_details))]
-        if FLAGS.model == 'yolov3' and FLAGS.tiny == True:
-            boxes, pred_conf = filter_boxes(pred[1], pred[0], score_threshold=0.25, input_shape=tf.constant([input_size, input_size]))
-        else:
-            boxes, pred_conf = filter_boxes(pred[0], pred[1], score_threshold=0.25, input_shape=tf.constant([input_size, input_size]))
+            interpreter = tf.lite.Interpreter(model_path=FLAGS.weights)
     else:
-        saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
-        infer = saved_model_loaded.signatures['serving_default']
-        batch_data = tf.constant(images_data)
-        pred_bbox = infer(batch_data)
-        for key, value in pred_bbox.items():
-            boxes = value[:, :, 0:4]
-            pred_conf = value[:, :, 4:]
+            saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
 
-    boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
-        boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
-        scores=tf.reshape(
-            pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
-        max_output_size_per_class=50,
-        max_total_size=50,
-        iou_threshold=FLAGS.iou,
-        score_threshold=FLAGS.score
-    )
-    pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
-    image = utils.draw_bbox(original_image, pred_bbox)
-    # image = utils.draw_bbox(image_data*255, pred_bbox)
-    image = Image.fromarray(image.astype(np.uint8))
-    image.show()
-    image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
-    cv2.imwrite(FLAGS.output, image)
+    # loop through images in list and run Yolov4 model on each
+    for count, image_path in enumerate(images, 1):
+        original_image = cv2.imread(image_path)
+        original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+
+        image_data = cv2.resize(original_image, (input_size, input_size))
+        image_data = image_data / 255.
+
+        images_data = []
+        for i in range(1):
+            images_data.append(image_data)
+        images_data = np.asarray(images_data).astype(np.float32)
+
+        if FLAGS.framework == 'tflite':
+            interpreter.allocate_tensors()
+            input_details = interpreter.get_input_details()
+            output_details = interpreter.get_output_details()
+            print(input_details)
+            print(output_details)
+            interpreter.set_tensor(input_details[0]['index'], images_data)
+            interpreter.invoke()
+            pred = [interpreter.get_tensor(output_details[i]['index']) for i in range(len(output_details))]
+            if FLAGS.model == 'yolov3' and FLAGS.tiny == True:
+                boxes, pred_conf = filter_boxes(pred[1], pred[0], score_threshold=0.25, input_shape=tf.constant([input_size, input_size]))
+            else:
+                boxes, pred_conf = filter_boxes(pred[0], pred[1], score_threshold=0.25, input_shape=tf.constant([input_size, input_size]))
+        else:
+            infer = saved_model_loaded.signatures['serving_default']
+            batch_data = tf.constant(images_data)
+            pred_bbox = infer(batch_data)
+            for key, value in pred_bbox.items():
+                boxes = value[:, :, 0:4]
+                pred_conf = value[:, :, 4:]
+
+        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+            scores=tf.reshape(
+                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+            max_output_size_per_class=50,
+            max_total_size=50,
+            iou_threshold=FLAGS.iou,
+            score_threshold=FLAGS.score
+        )
+        pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
+
+        # read in all class names from config
+        class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+
+        # by default allow all classes in .names file
+        allowed_classes = list(class_names.values())
+        
+        # custom allowed classes (uncomment line below to allow detections for only people)
+        #allowed_classes = ['person']
+
+        image = utils.draw_bbox(original_image, pred_bbox, allowed_classes = allowed_classes)
+
+        image = Image.fromarray(image.astype(np.uint8))
+        if not FLAGS.dont_show:
+            image.show()
+        image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+        cv2.imwrite(FLAGS.output + 'detection' + str(count) + '.png', image)
 
 if __name__ == '__main__':
     try:
