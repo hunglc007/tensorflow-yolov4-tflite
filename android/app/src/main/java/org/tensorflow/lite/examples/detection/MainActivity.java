75cb0325c7978001fc13f14f44fd298b91f0fe37
ky3vin <kimpika8541@naver.com>
2022-11-01 17:20:20 +0900

    사람만 탐지하는 모델 이식

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
@@ -204,7 +204,7 @@ public class MainActivity extends AppCompatActivity {
     private static final boolean TF_OD_API_IS_QUANTIZED = false;
 
     //private static final String TF_OD_API_MODEL_FILE = "yolov4-416-fp32.tflite";
-    private static final String TF_OD_API_MODEL_FILE = "yolov4-tiny-new-416.tflite";
+    private static final String TF_OD_API_MODEL_FILE = "yolov4-tiny-personOnly-416.tflite";
     private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/names.txt";
 
     // Minimum detection confidence to track a detection.
