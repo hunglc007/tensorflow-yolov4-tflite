ef2203f20cf18847e12b97c44804875d7d378941
ky3vin <kimpika8541@naver.com>
2022-11-04 15:25:37 +0900

    2000번 학습 모델 추가

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
@@ -46,7 +46,7 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.70f;
+    public static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.10f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final boolean TF_OD_API_IS_QUANTIZED = false;
 
-    private static final String TF_OD_API_MODEL_FILE = "yolov4-tiny-twoClass-416.tflite";
+    private static final String TF_OD_API_MODEL_FILE = "yolov4-tiny-twoClass-new-416.tflite";
     private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/names.txt";
     // Minimum detection confidence to track a detection.
     private static final boolean MAINTAIN_ASPECT = false;
